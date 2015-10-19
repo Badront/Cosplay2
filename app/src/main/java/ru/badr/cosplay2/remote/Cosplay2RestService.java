ae5fa03ab386c9ff39e802e4cca5e96e2dfbec32
Histler <histlerbar@gmail.com>
2015-10-19 18:36:04 +0300

    deleted todo

--- app/src/main/java/ru/badr/cosplay2/remote/Cosplay2RestService.java
+++ app/src/main/java/ru/badr/cosplay2/remote/Cosplay2RestService.java
@@ -11,7 +11,5 @@ import ru.badr.cosplay2.api.cards.TopicsAndCards;
 public interface Cosplay2RestService {
     @GET("/api/cards/get_topics_and_cards")
     TopicsAndCards getTopicsAndCards();
-    /*todo http://opencon14.cosplay2.ru/uploads/10 - {festival_id?}/{id}/{image_filename}_large.jpg
-    * or http://opencon14.cosplay2.ru/images/avatars/{user_id}.png*/
     /*todo http://opencon14.cosplay2.ru/api/cards/get_card?request_id={card_id}*/
 }
